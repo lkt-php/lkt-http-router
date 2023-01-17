c715b8db68ba2fa9767c71a500fb985d70fb1395
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2023-01-17 20:27:19 +0100

    Update dispatch method

--- src/Router.php
+++ src/Router.php
@@ -34,11 +34,18 @@ class Router
         static::$routes[$router][] = $route;
     }
 
-    public static function dispatch(string $router = 'default'): Response
+    public static function dispatch(string $router = 'default'): void
+    {
+        $response = static::getResponse($router);
+        $response->sendHeaders()->sendContent();
+        die();
+    }
+
+    public static function getResponse(string $router = 'default'): Response
     {
         /** @var AbstractRoute[] $routes */
         $routes = static::$routes[$router];
-        $dispatcher = simpleDispatcher(function(RouteCollector $r) use ($routes) {
+        $dispatcher = simpleDispatcher(function (RouteCollector $r) use ($routes) {
             foreach ($routes as $route) {
                 $r->addRoute($route->getMethod(), $route->getRoute(), [
                     'handler' => $route->getHandler(),
@@ -128,7 +135,7 @@ class Router
     {
         $requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
         $params = [];
-        
+
         // Merge variables
         $request = [];
         switch ($requestMethod) {
@@ -191,5 +198,36 @@ class Router
         }
 
         return $params;
+    }
+
+    private static function getAuthorizationHeader(): ?string
+    {
+        $headers = null;
+        if (isset($_SERVER['Authorization'])) {
+            $headers = trim($_SERVER["Authorization"]);
+        } else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+            $headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+        } elseif (function_exists('apache_request_headers')) {
+            $requestHeaders = apache_request_headers();
+            // Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+            $requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+            //print_r($requestHeaders);
+            if (isset($requestHeaders['Authorization'])) {
+                $headers = trim($requestHeaders['Authorization']);
+            }
+        }
+        return $headers;
+    }
+
+    public static function getBearerToken(): ?string
+    {
+        $headers = static::getAuthorizationHeader();
+        // HEADER: Get the access token from the header
+        if (!empty($headers)) {
+            if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+                return $matches[1];
+            }
+        }
+        return null;
     }
 }
