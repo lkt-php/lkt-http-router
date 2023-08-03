83821ebdf60fede229d322f66de3cbcd5a4a4e8a
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2023-08-03 20:52:12 +0200

    Prevent access to SERVER data if called from cli

--- src/Networking/Networking.php
+++ src/Networking/Networking.php
@@ -18,36 +18,38 @@ final class Networking
 
     public function __construct()
     {
-        $remoteAddr = $_SERVER['REMOTE_ADDR'];
+        if (php_sapi_name() !== 'cli') {
+            $remoteAddr = $_SERVER['REMOTE_ADDR'];
 
-        if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] > 0) {
-            $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
-            $remoteAddr = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && isset($_SERVER['HTTP_X_FORWARDED_FOR']) && $_SERVER['HTTP_X_FORWARDED_FOR'] > 0) {
+                $_SERVER['REMOTE_ADDR'] = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                $remoteAddr = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
+            $this->remoteAddress = $remoteAddr;
+
+            $httpXForwardedProto = '';
+            if (array_key_exists('HTTP_X_FORWARDED_PROTO', $_SERVER) && isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+                $_SERVER['HTTPS'] = 'on';
+                $_SERVER['SERVER_PORT'] = '443';
+                $httpXForwardedProto = 'https';
+            }
+
+            $this->httpProtocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) || ($httpXForwardedProto === 'https') ? 'https' : 'http';
+
+            $protocol = 'HTTP/1.0';
+            if ('HTTP/1.1' == $_SERVER['SERVER_PROTOCOL']) $protocol = 'HTTP/1.1';
+            $this->httpProtocolVersion = $protocol;
+
+            $this->httpHost = "{$this->httpProtocol}://{$_SERVER['HTTP_HOST']}";
+            $this->requestUri = $_SERVER['REQUEST_URI'];
+
+            $uri = explode('?', $this->requestUri);
+            $this->cleanedRequestUri = $uri[0];
+
+            $this->serverName = $_SERVER['SERVER_NAME'];
+
+            $this->requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
         }
-        $this->remoteAddress = $remoteAddr;
-
-        $httpXForwardedProto = '';
-        if (array_key_exists('HTTP_X_FORWARDED_PROTO', $_SERVER) && isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-            $_SERVER['HTTPS'] = 'on';
-            $_SERVER['SERVER_PORT'] = '443';
-            $httpXForwardedProto = 'https';
-        }
-
-        $this->httpProtocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) || ($httpXForwardedProto === 'https') ? 'https' : 'http';
-
-        $protocol = 'HTTP/1.0';
-        if ('HTTP/1.1' == $_SERVER['SERVER_PROTOCOL']) $protocol = 'HTTP/1.1';
-        $this->httpProtocolVersion = $protocol;
-
-        $this->httpHost = "{$this->httpProtocol}://{$_SERVER['HTTP_HOST']}";
-        $this->requestUri = $_SERVER['REQUEST_URI'];
-
-        $uri = explode('?', $this->requestUri);
-        $this->cleanedRequestUri = $uri[0];
-
-        $this->serverName = $_SERVER['SERVER_NAME'];
-
-        $this->requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
     }
 
     private static function getInstance(): self
