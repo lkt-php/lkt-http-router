9e6cb1857cf3feac43b9e6ef2472c9625a07a44a
Antonio Ibáñez <am.ibanez.costa@gmail.com>
2026-01-21 18:45:45 +0100

    Notification: support for success, fail and warning toast

--- src/Notification.php
+++ src/Notification.php
@@ -9,8 +9,17 @@ class Notification
 {
     public NotificationCategory $category = NotificationCategory::Toast;
 
+    public static string $defaultSuccessIcon = '';
+    public static string $defaultFailIcon = '';
+    public static string $defaultWarningIcon = '';
+
+    public static string $defaultSuccessClass = '';
+    public static string $defaultFailClass = '';
+    public static string $defaultWarningClass = '';
+
     public string $text = '';
     public string $details = '';
+    public string $class = '';
 
     public string $icon = '';
 //    public string $positionX = '';
@@ -19,6 +28,7 @@ class Notification
     {
         $this->category = $category;
         if ($payload['text']) $this->text = clearInput($payload['text']);
+        if ($payload['class']) $this->class = clearInput($payload['class']);
         if ($payload['details']) $this->details = clearInput($payload['details']);
         if ($payload['icon']) $this->icon = clearInput($payload['icon']);
 //        if ($payload['positionX']) $this->details = clearInput($payload['positionX']);
@@ -31,11 +41,45 @@ class Notification
         return $instance;
     }
 
+    public static function sendSuccessToast(array $payload): static
+    {
+        $p = [];
+        if (static::$defaultSuccessClass) $p['class'] = static::$defaultSuccessClass;
+        if (static::$defaultSuccessIcon) $p['icon'] = static::$defaultSuccessIcon;
+
+        $instance = new static( NotificationCategory::Toast, [...$p, ...$payload]);
+        Router::addPendingNotification($instance);
+        return $instance;
+    }
+
+    public static function sendFailToast(array $payload): static
+    {
+        $p = [];
+        if (static::$defaultFailClass) $p['class'] = static::$defaultFailClass;
+        if (static::$defaultFailIcon) $p['icon'] = static::$defaultFailIcon;
+
+        $instance = new static( NotificationCategory::Toast, [...$p, ...$payload]);
+        Router::addPendingNotification($instance);
+        return $instance;
+    }
+
+    public static function sendWarningToast(array $payload): static
+    {
+        $p = [];
+        if (static::$defaultWarningClass) $p['class'] = static::$defaultWarningClass;
+        if (static::$defaultWarningIcon) $p['icon'] = static::$defaultWarningIcon;
+
+        $instance = new static( NotificationCategory::Toast, [...$p, ...$payload]);
+        Router::addPendingNotification($instance);
+        return $instance;
+    }
+
     public function toArray(): array
     {
         $payload = [];
         if ($this->text !== '') $payload['text'] = $this->text;
         if ($this->details !== '') $payload['details'] = $this->details;
+        if ($this->class !== '') $payload['class'] = $this->class;
         if ($this->icon !== '') $payload['icon'] = $this->icon;
         return [
             'category' => $this->category->value,
